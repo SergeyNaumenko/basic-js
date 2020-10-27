f5a4ec70df1fec1c3a5c878bfee9b034555d342e
Sergey <sergeynau@gmail.com>
2020-10-27 12:52:38 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,27 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+/* use 10th formula from 
+*https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Nuclear_Chemistry/Nuclear_Kinetics/Half-Lives_and_Radioactive_Decay_Kinetics#mjx-eqn-21.4.6
+*/
+module.exports = function dateSample(sampleActivity) {
+  if (!isValid()) {
+    return false;
+  }
+ 
+  const k = Math.LN2 / HALF_LIFE_PERIOD;
+  const numberOfAtomsRelationship = MODERN_ACTIVITY / parseFloat(sampleActivity);
+  const age = Math.log10(numberOfAtomsRelationship)/k;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return Math.ceil(age);
 };
+
+const isValid = (data) => {
+  if (typeof data !== 'string'){
+    return false;
+  }
+  if(isNaN(parseFloat(data))) {
+    return false;
+  }
+  return true;
+}
