f2ca41e6d8c0b3781302a5e136e548464af7bf3f
Sergey <sergeynau@gmail.com>
2020-10-30 12:34:38 +0300

    feat: complete basic js task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options = {}) {
+  let {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+  } = options;
+
+  separator = separator || `+`;
+  additionSeparator = additionSeparator || `|`;
+  addition = (addition === null) ? `null` : addition;
+
+  const additionStr = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+  return new Array(repeatTimes).fill(`${str}${additionStr}`).join(separator);
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(option) {
+    this.reverseStr = (option === false);
+    this.latinAlphabetLength = 26;
+    this.shift = 65;  //for example A from 65 ascii to 0
+  }
+
+  encrypt(message , key) {
+    this.isValid(message , key);
+    let result = ''
+    
+    for(let i = 0, j = 0; i < message.length; i++) {
+
+      const char = message[i].toUpperCase();
+      
+      if (!this.isLatinLetter(char)){
+        result += char;
+      } else {
+        const charIndex = char.charCodeAt();
+        const keyChar = key[ j%key.length ].toUpperCase();
+        const keyIndex = keyChar.charCodeAt();
+        
+        let encryptedLetterIndex = ( (charIndex - this.shift) +  (keyIndex - this.shift)) % this.latinAlphabetLength;
+        
+        result += String.fromCharCode(encryptedLetterIndex + this.shift);
+        j++;
+      }
+    }
+    return this.reverseStr ? [...result].reverse().join('') : result;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    this.isValid(message , key);
+
+    let result = ''
+    
+    for(let i = 0, j = 0; i < message.length; i++) {
+
+      const char = message[i].toUpperCase();
+      
+      if (!this.isLatinLetter(char)){
+        result += char;
+      } else {
+        const charIndex = char.charCodeAt();
+        const keyChar = key[ j%key.length ].toUpperCase();
+        const keyIndex = keyChar.charCodeAt();
+        
+        let encryptedLetterIndex = ( (charIndex - this.shift) + (this.latinAlphabetLength - keyIndex + this.shift)) % this.latinAlphabetLength;
+        
+        result += String.fromCharCode(encryptedLetterIndex + this.shift);
+        j++;
+      }
+    }
+    return this.reverseStr ? [...result].reverse().join('') : result;
+  }
+
+  isValid(message, key) {
+    if (!message) {
+      throw new Error(`The message is empty`);
+    } 
+    if (!key){
+      throw new Error(`The key is empty`);
+    }
+  }
+
+  /*Is Latin in Upper case from ASCII table
+  */
+  isLatinLetter(char) {
+    const charIndex = char.charCodeAt();
+    return charIndex >= 65 && charIndex <= 90 ? true : false;
   }
 }
 
